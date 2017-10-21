cbc8eb251b580e245c43ee644952f15d9e05b124
lgants <lgants@gmail.com>
2017-10-20 22:07:33 -0500

    added authentication

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,9 +1,8 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # queries are just represented as fields
-  field :allLinks, !types[Types::LinkType] do
-    # resolve would be called in order to fetch data for that field
-    resolve -> (obj, args, ctx) { Link.all }
+  field :createLink, function: Resolvers::CreateLink.new
+  field :createUser, function: Resolvers::CreateUser.new
+  field :signinUser, function: Resolvers::SignInUser.new
   end
 end
